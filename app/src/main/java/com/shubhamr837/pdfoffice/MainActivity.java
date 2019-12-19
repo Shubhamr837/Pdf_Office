d3ccc674ef18fa75040eac5c6462cc06d63fbf7a
shubham <shubhamr837@gmail.com>
2019-12-19 11:08:00 +0530

    Added Gridview

--- app/src/main/java/com/shubhamr837/pdfoffice/MainActivity.java
+++ app/src/main/java/com/shubhamr837/pdfoffice/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.shubhamr837.pdfoffice.adapters.ImageAdapter;
 import com.shubhamr837.pdfoffice.ui.EmailPasswordActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
         user = FirebaseAuth.getInstance().getCurrentUser();
         if(user==null)
             authenticate();
